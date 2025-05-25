55de85999764ec3d9bdb20b6c2833bf999ce6c40
keyoverlayfinder <jaxsonrb@gmail.com>
2025-05-25 13:39:38 -0400

    My name is Jaxson Riley Brewer, I am 18 years old, I love long walks by the beach and playing with my dog! I will be attending Harvard University

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/autonomous/IntoTheDeep/Learn/Claw.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.autonomous.IntoTheDeep.Learn;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Claw {
+    private final Servo clawLeft, clawRight;
+
+    public Claw(HardwareMap hardwareMap) {
+        clawLeft = hardwareMap.get(Servo.class, "leftClaw");
+        clawRight = hardwareMap.get(Servo.class, "rightClaw");
+        clawLeft.setDirection(Servo.Direction.REVERSE);
+    }
+
+    public class CloseClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double closedPos = 0.65;
+            clawLeft.setPosition(closedPos);
+            clawRight.setPosition(closedPos);
+            return false;
+        }
+    }
+
+    public Action closeClaw() {
+        return new CloseClaw();
+    }
+
+    public class OpenClaw implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double openPos = 0.4;
+            clawLeft.setPosition(openPos);
+            clawRight.setPosition(openPos);
+            return false;
+        }
+    }
+
+    public Action openClaw() {
+        return new OpenClaw();
+    }
+}
+
